56a3f723d5a38612f258f28f8f765e265e685a2c
keelbeelveel <k.belveal17@gmail.com>
2023-04-18 14:45:27 -0400

    refined run.sh

--- FastSimulation/PIMSim/run.sh
+++ FastSimulation/PIMSim/run.sh
@@ -1,2 +1,41 @@
 #!/bin/bash
-dotnet run -t /examples/1cpu1pu_function_detail/ -config /examples/1cpu1pu_function_detail/ -o /examples/1cpu1pu_function_detail/output.txt -n 1 -c 100
+
+BG=0
+
+if [ "$#" -gt 2 ]; then
+    BG=1
+fi
+
+ChkPath() {
+    if $# -le 2; then
+        echo "Must specify path to input directory."
+        exit 1
+    fi
+}
+
+PREFIX="/"
+
+case $1 in
+    *"-e"*)
+        ChkPath
+        PREFIX="/examples/"
+        ;;
+    *"-t"*)
+        ChkPath
+        PREFIX="/tests/"
+        ;;
+esac
+
+while (( "$#" )); do
+
+    INPATH="${PREFIX}$1"
+    if [ -d "$INPATH"]; then
+        if [ $BG -eq 1 ]; then
+            dotnet run -t $INPATH -config $INPATH -o $INPATH -n 1 -c 100 &
+        else 
+            dotnet run -t $INPATH -config $INPATH -o $INPATH -n 1 -c 100
+        fi
+    fi
+    shift
+done
+
